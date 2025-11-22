c778d26d48c8086c4527b0582d6a422f2c796629
Tomasz Korytko <tkorytko@c4r1s7.42warsaw.pl>
2025-11-22 20:05:35 +0100

    getnextline 0.2

--- Circle 01/get_next_line/get_next_line.c
+++ Circle 01/get_next_line/get_next_line.c
@@ -6,13 +6,58 @@
 /*   By: tkorytko <tkorytko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/16 15:03:59 by tkorytko          #+#    #+#             */
-/*   Updated: 2025/11/16 15:48:10 by tkorytko         ###   ########.fr       */
+/*   Updated: 2025/11/22 19:54:24 by tkorytko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+static char	*fill_buffer(int fd, char *left_c, char *buffer)
 {
-	
+	int			n;
+	char		*temp;
+
+	n = 1;
+	while (n > 0)
+	{
+		n = read(fd, buffer, 5);
+		if (n == -1)
+		{
+			free(left_c);
+			return (NULL);
+		}
+		if (n == 0)
+			break ;
+		buffer[n] = 0;
+		if (!left_c)
+			left_c = ft_strdup("");
+		temp = left_c;
+		left_c = ft_strjoin(temp, buffer);
+		free(temp);
+		temp = NULL;
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	return (left_c);
+}
+
+char	*set_line(char	*line_buffer)
+{
+	size_t		i;
+	char	*line;
+
+	i = 0;
+	while (line_buffer[i] != '\n' && line_buffer[i] != '\0')
+		i++;
+	line = malloc(i + 2);
+	if (!line)
+		return (NULL);
+	line = ft_substr(line_buffer, i + 1, ft_strlen(line_buffer) - i);
+	if (*line == 0)
+	{
+		free(line);
+		line = NULL;
+	}
+	line_buffer[i + 1] = 0;
+	return (line);
 }
--- Circle 01/get_next_line/get_next_line.h
+++ Circle 01/get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: tkorytko <tkorytko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/16 15:07:57 by tkorytko          #+#    #+#             */
-/*   Updated: 2025/11/16 15:48:00 by tkorytko         ###   ########.fr       */
+/*   Updated: 2025/11/22 17:19:11 by tkorytko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 
 char	*get_next_line(int fd);
-char	*fill_line_buffer(int fd, char *left_c, char *buffer);
-char	*_set_line(char *line_buffer);
+char	*fill_buffer(int fd, char *left_c, char *buffer);
+char	*set_line(char *line_buffer);
 
 #endif // GET_NEXT_LINE_H
